765b95a9d17713a50dbee03b4d406d443330d653
feliksczart <czart78@gmail.com>
2021-01-31 13:56:41 +0100

    client: no server running window

--- client/main/Client.java
+++ client/main/Client.java
@@ -4,17 +4,27 @@ import gui.GameWindow;
 import mainGame.Game;
 import serverConnection.Messenger;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.net.ConnectException;
+
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        Messenger messenger = new Messenger(1111);
-        Game game = new Game(messenger);
-        GameWindow gameWindow = new GameWindow(game);
-        game.setGameWindow(gameWindow);
-        messenger.setGame(game);
-        //uruchamiamy wątek, który na bieżąco odczytuje wiadomości z serwera
-        messenger.startReadThread();
+        try{
+            Messenger messenger = new Messenger(1111);
+            Game game = new Game(messenger);
+            GameWindow gameWindow = new GameWindow(game);
+            game.setGameWindow(gameWindow);
+            messenger.setGame(game);
+            //uruchamiamy wątek, który na bieżąco odczytuje wiadomości z serwera
+            messenger.startReadThread();
+        } catch (ConnectException ce){
+            showMessageDialog(null, "No server running", "", ERROR_MESSAGE);
+            System.exit(1);
+        }
     }
 }
--- client/main/Client2.java
+++ client/main/Client2.java
@@ -5,15 +5,24 @@ import mainGame.Game;
 import serverConnection.Messenger;
 
 import java.io.IOException;
+import java.net.ConnectException;
+
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Client2 {
 
     public static void main(String[] args) throws IOException {
-        Messenger messenger = new Messenger(1111);
-        Game game = new Game(messenger);
-        GameWindow gameWindow = new GameWindow(game);
-        game.setGameWindow(gameWindow);
-        messenger.setGame(game);
-        messenger.startReadThread();
+        try{
+            Messenger messenger = new Messenger(1111);
+            Game game = new Game(messenger);
+            GameWindow gameWindow = new GameWindow(game);
+            game.setGameWindow(gameWindow);
+            messenger.setGame(game);
+            messenger.startReadThread();
+        } catch (ConnectException ce){
+            showMessageDialog(null, "No server running", "", ERROR_MESSAGE);
+            System.exit(1);
+        }
     }
 }
